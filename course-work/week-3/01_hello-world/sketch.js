aed99350c5681c9d41cc7cee32bc6b80e304095c
Jacqueline Evans-Shaw <39539456+Jackie98@users.noreply.github.com>
2018-09-06 21:49:38 -0600

    HW3

---
+++ course-work/week-3/01_hello-world/sketch.js
@@ -0,0 +1,6 @@
+function setup(){
+    text("Hello Jackie!", 0, 10);
+}
+function setup(){
+    ellipse(20, 20, 30, 30);
+}
---
+++ hw-3/sketch.js
@@ -0,0 +1,14 @@
+// Declare the setup function
+function setup() {
+    // create a canvas 600px wide and 400px high
+    createCanvas( 500, 300 );
+
+    // color the background 'pink'
+    background( 'blue' );
+}
+// Declare the draw function
+function draw() {
+    // tell p5 to print the text string "Hello World!"
+    // This is going to be positioned at ( x: 20, y: 30 ).
+    text( "Hello World!", 20, 30 );
+}
